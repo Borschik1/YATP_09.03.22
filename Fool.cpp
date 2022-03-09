42b12866dc864ce4c360964821eb73488c82e280
Borschik <47531847+Borschik1@users.noreply.github.com>
2022-03-09 20:17:24 +0500

    Update Fool.cpp

--- Fool.cpp
+++ Fool.cpp
@@ -12,6 +12,9 @@ public:
 class Player {
 public:
     Card* cards;
+
+    void add_card(Card card) {
+    }
 };
 
 class Game {
@@ -19,7 +22,7 @@ public:
     int trump;
 
     void round(Player attacter, Player defenser, int* card1, int* card2) {
-        //Ôóíêöèÿ áóäåò ñìîòðåòü, ïîêðûâàþò ëè êàðòû 2 èãðîêà êàðòû ïåðâîãî è â çàâèñèìîñòè îò ðåçóëüòàòà óáèðàòü êàðòû èç ðóê èãðîêîâ èëè âûäàâàòü îøèáêó
+
     }
 };
 
